74bf3e669e6a547360610577420a6e2c3c0d6b93
Amanowsky <adamtroszczynski@outlook.com>
2024-02-08 16:09:30 +0100

    update readme.md, added overflow hidden to mainwrapper

--- README.md
+++ README.md
@@ -24,7 +24,7 @@ Users should be able to:
 
 ### Links
 
-comming soon...
+- Live Site URL: [Add live site URL here](https://multi-step-form-at.netlify.app/)
 
 ## My process
 
--- src/components/layout/MainWrapper.vue
+++ src/components/layout/MainWrapper.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="w-screen h-screen flex flex-col items-center xl:w-4/6 xl:h-3/4 xl:bg-white xl:rounded-2xl xl:p-5 xl:flex-row xl:justify-between">
+    <div class="w-screen h-screen flex flex-col items-center xl:w-4/6 xl:h-3/4 xl:bg-white xl:rounded-2xl xl:p-5 xl:flex-row xl:justify-between overflow-hidden">
         <slot></slot>
     </div>
 </template>
